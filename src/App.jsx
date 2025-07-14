31d5448c321fbfe65f426362e116c23703834bad
DaniLucano10 <olucano10@gmail.com>
2025-07-14 17:24:54 -0500

    ordenando las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AdminLayout } from "./layouts/AdminLayout";
 import { AuthLayout } from "./layouts/AuthLayout";
@@ -10,26 +8,36 @@ import { Error404 } from "./pages/Error404";
 import { Login } from "./pages/auth/Login";
 import { Register } from "./pages/auth/Register";
 import { ForgetPassword } from "./pages/auth/ForgetPassword";
+import { PrivateRoute } from "./PrivateRoute";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AdminLayout />}>
-            <Route index element={<Home />} />
-            <Route path="chat" element={<Chat />} />
-            <Route path="users" element={<Users />} />
-          </Route>
-          <Route path="/auth" element={<AuthLayout />}>
-            <Route index element={<Login />} />
-            <Route path="registro" element={<Register />} />
-            <Route path="olvide-password" element={<ForgetPassword />} />
-          </Route>
-          <Route path="*" element={<Error404 />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+         {/* Rutas públicas */}
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path="registro" element={<Register />} />
+          <Route path="olvide-password" element={<ForgetPassword />} />
+        </Route>
+        
+        {/* Rutas protegidas */}
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <AdminLayout />
+            </PrivateRoute>
+          }
+        >
+          <Route index element={<Home />} />
+          <Route path="chat" element={<Chat />} />
+          <Route path="users" element={<Users />} />
+        </Route>
+
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
