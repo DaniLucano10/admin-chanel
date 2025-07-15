272ef60a8c9777014a49e97e10f37039d6abd102
DaniLucano10 <olucano10@gmail.com>
2025-07-15 11:58:12 -0500

    hook para listar usuarios

---
+++ src/hooks/users/useFetchUsers.jsx
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useMemo, useState } from "react"
+import { useAuth } from "../useAuth";
+import axios from "axios";
+
+
+export const useFetchUsers = (params) => {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const { getToken } = useAuth();
+
+    const memoizedParams = useMemo(() => params, [params]);
+
+    const fetchUsers = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const token = getToken();
+            const response = await axios.get(import.meta.env.VITE_API_URL + '/users', {
+                headers: { Authorization: `Bearer ${token}`},
+                params: memoizedParams,
+            });
+            setData(response.data);
+        } catch (err) {
+            setData([]);
+            setError(
+                err.response
+                ? err.response.data
+                : 'Ha ocurrido un error al obtener los datos'
+            );
+        } finally {
+            setLoading(false);
+        }
+    }, [memoizedParams, getToken]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, [fetchUsers]);
+
+    return { loading, data, error, fetchUsers}
+}
