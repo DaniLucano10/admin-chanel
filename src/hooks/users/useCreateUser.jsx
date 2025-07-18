ae06b8abcbeb4be614bf2f98215f47dd23635165
DaniLucano10 <olucano10@gmail.com>
2025-07-18 12:29:56 -0500

    agregando succes

--- src/hooks/users/useCreateUser.jsx
+++ src/hooks/users/useCreateUser.jsx
@@ -3,39 +3,44 @@ import { useAuth } from './../useAuth';
 import axios from "axios";
 
 
-export const useCreateUser = ({ fetch, close}) => {
+export const useCreateUser = ({ fetch, close }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
   const [data, setData] = useState(null);
   const { getToken } = useAuth();
 
   const register = async (formData) => {
     setLoading(true);
     setError(null);
+    setSuccess(null);
 
     try {
-        const token = getToken();
+      const token = getToken();
         const response = await axios.post(
-            import.meta.env.VITE_API_URL + '/user',
+            import.meta.env.VITE_API_URL  + '/users',
             formData,
             {
                 headers: { Authorization: `Bearer ${token}`},
             }
         );
         setData(response.data);
+        setSuccess("Usuario creado exitosamente.");
         fetch();
-        close();
+        setTimeout(() => {
+            close();
+            setSuccess(null);
+        }, 2000);
     } catch (err) {
         setError(
-            err.response
-            ? err.response.data
-            : 'A ocurrido un erre al regitrar los datos.'
+            err.response && err.response.data
+            ? err.response.data.message
+            : "Ocurrió un error al registrar los datos."
         );
     } finally {
         setLoading(false);
     }
   };
 
-  return { register, data, loading, error, setError}
-}
-
+  return { register, data, loading, error, success, setError };
+};
