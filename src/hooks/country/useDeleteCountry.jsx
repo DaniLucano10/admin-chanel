085aceef96d5ebdc1f603ce5b0e47e01b7e2ec0e
DaniLucano10 <olucano10@gmail.com>
2025-07-21 17:22:13 -0500

    hook para eliminar pais

---
+++ src/hooks/country/useDeleteCountry.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useAuth } from "../useAuth";
+import axios from "axios";
+
+export const useDeleteCountry = ({ id, fetch, close }) => {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const { getToken } = useAuth();
+
+    const remove = async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const token = getToken();
+            const response = await axios.delete(
+                `${import.meta.env.VITE_API_URL}/country/${id}`,
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+            );
+            setData(response.data);
+            fetch();
+            close();
+        } catch (err) {
+            setError(
+                err.response
+                    ? err.response.data
+                    : 'A ocurrido un error al eliminar el dato.'
+            );
+            close();
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { remove, loading, error, data };
+};
