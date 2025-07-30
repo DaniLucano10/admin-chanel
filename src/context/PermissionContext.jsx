059c987ede6df866ea92fed0a90a025302b6e008
DaniLucano10 <olucano10@gmail.com>
2025-07-30 11:10:48 -0500

    permiso context

---
+++ src/context/PermissionContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { useReadRolesAndPermissions } from '../hooks/users/useReadRolesAndPermissions';
+
+const PermissionContext = createContext();
+
+export const usePermissions = () => {
+	const context = useContext(PermissionContext);
+	if (!context) {
+		throw new Error(
+			'usePermissions debe ser utilizado dentro de un PermissionProvider'
+		);
+	}
+	return context;
+};
+
+export const PermissionProvider = ({ children }) => {
+	const { permissions, loading, error } = useReadRolesAndPermissions();
+
+	return (
+		<PermissionContext.Provider value={{ permissions, loading, error }}>
+			{children}
+		</PermissionContext.Provider>
+	);
+};
+
+PermissionProvider.propTypes = {
+	children: PropTypes.node.isRequired,
+};
