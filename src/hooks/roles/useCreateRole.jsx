e98a2f6c831635405622b91514cd61c519d93689
DaniLucano10 <olucano10@gmail.com>
2025-07-30 14:45:31 -0500

    hook para crear rol

--- src/hooks/roles/useCreateRole.jsx
+++ src/hooks/roles/useCreateRole.jsx
@@ -1,39 +1,35 @@
 import { useState } from "react";
 import { useAuth } from "../useAuth";
 import axios from "axios";
+import { useToast } from "../../context/ToastContext";
 
 export const useCreateRole = ({ fetch, close }) => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [data, setData] = useState(null);
-    const { getToken } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { getToken } = useAuth();
+  const { showToast } = useToast();
 
-    const register = async (formData) => {
-        setLoading(true);
-        setError(null);
+  const register = async (formData) => {
+    setLoading(true);
+    setError(null);
 
-        try {
-            const token = getToken();
-            const response = await axios.post(
-                `${import.meta.env.VITE_API_URL}/role`,
-                formData,
-                {
-                    headers: { Authorization: `Bearer ${token}` },
-                }
-            );
-            setData(response.data);
-            fetch();
-            close();
-        } catch (err) {
-            setError(
-                err.response
-                    ? err.response.data
-                    : 'A ocurrido un error al registrar el dato.'
-            );
-        } finally {
-            setLoading(false);
-        }
-    };
+    try {
+      const token = getToken();
+      await axios.post(`${import.meta.env.VITE_API_URL}/role`, formData, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      showToast("Rol creado correctamente", "success");
+      fetch();
+      close();
+    } catch (err) {
+      const errorMessage =
+        err.response?.data?.message || "Ocurrió un error al registrar rol.";
+      setError({ message: errorMessage });
+      showToast(errorMessage, "error");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return { register, data, loading, error, setError };
-}
+  return { register, loading, error, setError };
+};
